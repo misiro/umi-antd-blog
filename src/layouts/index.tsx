15699eeef0d8d715680787970843735d91498d0f
misiro <OnlyRailgun@outlook.com>
2022-07-10 00:48:39 +0800

    chore(release): update changelog

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,48 @@
-import {Outlet} from 'umi';
+import { EditOutlined, HomeOutlined, LoginOutlined } from '@ant-design/icons';
+import { Layout, Menu, MenuProps } from 'antd';
+import { Content } from 'antd/lib/layout/layout';
+import { useState } from 'react';
+import { history, Outlet } from 'umi';
 
-export default function Layout() {
-    return (
-        <div>
-            <Outlet/>
-        </div>
-    );
-}
+const items: MenuProps['items'] = [
+  {
+    label: '主页',
+    key: '/',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: '发表文章',
+    key: '/posts/create',
+    icon: <EditOutlined />,
+  },
+  {
+    label: '登录',
+    key: '/login',
+    icon: <LoginOutlined />,
+  },
+];
+
+const App: React.FC = () => {
+  const [current, setCurrent] = useState('/');
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    setCurrent(e.key);
+    history.push(e.key);
+  };
+
+  return (
+    <Layout>
+      <Menu
+        onClick={onClick}
+        selectedKeys={[current]}
+        mode="horizontal"
+        items={items}
+      />
+      <Content style={{ padding: '0 50px' }}>
+        <Outlet></Outlet>
+      </Content>
+    </Layout>
+  );
+};
+
+export default App;
